6edc8f0c9d75289b586bebd66b1405bdfe656935
Bianca Cervantes <cervbianca9810@gmail.com>
2021-10-13 16:44:33 -0700

    float to int

--- multiply.py
+++ multiply.py
@@ -1,7 +1,6 @@
 def multiply_list(mylist):
-    result = int(1)
+    result = float(1)
     for i in mylist:
-        result =int(i) *int(result)
+        result =float(i) *float(result)
+        result=int(result)
     return result
-
-print(multiply_list([1,2,3,7]))
