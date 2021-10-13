ecf14dddd30bfe7b2c0636613b331bc2b4e0fb02
Bianca Cervantes <cervbianca9810@gmail.com>
2021-10-13 12:24:21 -0700

    changes to fix error

--- multiply.py
+++ multiply.py
@@ -1,5 +1,7 @@
 def multiply_list(mylist):
     result = 1
     for i in mylist:
-        result = int(i)* int(result)
+        result = int(i)* result
     return result
+
+print(multiply_list([1,2,3,7]))
