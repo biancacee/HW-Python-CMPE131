d47fd429276dd9f7b178c35f294db23b99a5c75e
Bianca Cervantes <cervbianca9810@gmail.com>
2021-10-13 15:41:28 -0700

    fixed issue with output

--- timing.py
+++ timing.py
@@ -1,16 +1,12 @@
 import time
 
 def calculate_time(func):
-    def test_decorator():
         start_time= time.time()
         test_function()
         end_time=time.time()
         total_time= end_time-start_time
         print ("Total time ",total_time)
-    return test_decorator
 def test_function():
-    return time.sleep(2)
-    
+    time.sleep(2)
 
-final_runtime=calculate_time(test_function)
-final_runtime()
+time_funtion=calculate_time(test_function)
