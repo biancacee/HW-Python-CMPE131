5baa7f35772582aafc3646c5e27b3b2e3d145859
Bianca Cervantes <cervbianca9810@gmail.com>
2021-10-13 16:39:38 -0700

    changed int * int

--- multiply.py
+++ multiply.py
@@ -1,5 +1,7 @@
 def multiply_list(mylist):
     result = int(1)
     for i in mylist:
-        result = i * result
+        result =int(i) *int(result)
     return result
+
+print(multiply_list([1,2,3,7]))
