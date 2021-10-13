da1826951be9723f6a9dfcfa17dd736a9aacaf31
Bianca Cervantes <cervbianca9810@gmail.com>
2021-10-13 16:47:39 -0700

    fixed yet another error

--- multiply.py
+++ multiply.py
@@ -1,6 +1,7 @@
 def multiply_list(mylist):
-    result = float(1)
+    result = 1
     for i in mylist:
         result =float(i) *float(result)
         result=int(result)
     return result
+print(multiply_list([1,2,3,7]))
