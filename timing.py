96dca332b19d982e1c73c4196953324ae60a2acc
Bianca Cervantes <cervbianca9810@gmail.com>
2021-10-13 16:25:55 -0700

    decorator issues fixed, hopefully

--- timing.py
+++ timing.py
@@ -1,12 +1,16 @@
 import time
 
-def calculate_time(func):
+def calculate_time(test_function):
+    def wrapper():
         start_time= time.time()
         test_function()
         end_time=time.time()
         total_time= end_time-start_time
-        print ("Total time {}".format(total_time))
-def test_function():
+        print("Total time is ", total_time)
+    return wrapper
+
+def function_runtime():
     time.sleep(2)
 
-calculate_time(test_function)
+function_runtime=calculate_time(function_runtime)
+function_runtime()
