4a3eddb9e0979594abcedc547fb8f0011569ad93
Bianca Cervantes <cervbianca9810@gmail.com>
2021-10-14 12:05:33 -0700

    fixed docstring final submission

--- multiply.py
+++ multiply.py
@@ -1,9 +1,7 @@
 mylist=[]
 
 def multiply_list(mylist):
-    """This function takes a list and multiplies the elements 
-       within the list. Upon completion the function returns the result of 
-       the multiplies elements in the list.
+    """This function takes a list and multiplies the elements within the list. Upon completion the function returns the result of the multiplies elements in the list.
     """
     result=1 
     for i in mylist:
