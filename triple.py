5e926c3c10086254bf537a91cc564704165f075b
Bianca Cervantes <cervbianca9810@gmail.com>
2021-10-14 11:51:26 -0700

    final submission

--- triple.py
+++ triple.py
@@ -1,5 +1,8 @@
 def tripler(func):
     def wrapper_tripler():
+        """A decorator that can be used in other functions. This decorator
+           is passed a function and contains a nested function which calls            the function that is passed three times.
+        """
         func()
         func()
         func()
